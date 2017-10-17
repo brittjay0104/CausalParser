145e6e7b61242b9c741309a4ea083473cba127df
Brittany Johnson <bijohnsonphd@gmail.com>
2017-10-16 21:01:07 -0400

    Adds stub plug-in for causal testing

---
+++ Annotation/MyMarker/src/edu/umass/code_parser/Parser.java
@@ -0,0 +1,10 @@
+package edu.umass.code_parser;
+
+public class Parser {
+	
+	// failed test command (e.g. assertEqual)
+	// called methods
+	// hashmap of methods and important variables
+	// hashmap of variables and values
+
+}
